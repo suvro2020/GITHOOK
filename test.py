e765e0d708aabcad0936fb6d309725bc60196efd
suvro2020 <imranul.suvro@gmail.com>
2024-12-27 23:15:09 +0600

    Updated

---
+++ test.py
@@ -0,0 +1,11 @@
+n = int(input("Enter an integer number: "))
+
+# Determine the output based on divisibility
+if n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz")
+elif n % 3 == 0:
+    print("Fizz")
+elif n % 5 == 0:
+    print("Buzz")
+else:
+    print("xyz")
