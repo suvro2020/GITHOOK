b2cfc19d4d6447fac5c8740ae88850401c59d547
suvro2020 <imranul.suvro@gmail.com>
2024-12-28 20:44:07 +0600

    New Feature Added

---
+++ list_data.py
@@ -0,0 +1,13 @@
+# Position / Index ->> 0
+
+my_list = ['suvro', 'sobuj', 'shakil']
+print(my_list[-1])
+
+my_list = ['suvro', 'sobuj', 'shakil']
+my_list.append ('ifty')
+print(my_list)
+
+my_list1 = ['suvro', 'sobuj', 'shakil']
+my_list2 = ['sujon', 'sojib', 'sayem']
+new_group = my_list1+my_list2
+print (new_group)
---
+++ loops.py
@@ -0,0 +1,18 @@
+fruits = ['apple', 'stawberry', 'banana', 'orange', 'grape']
+
+# Iteration 
+# Loops -> for, while
+
+
+# for Loop
+
+# for item in fruits:
+#     print (item)
+
+fruits = ['apple', 'stawberry', 'banana', 'orange', 'grape']
+for fruit in fruits:
+    if fruit == 'grape':
+        print('grape found')
+    else:
+        print('Fruits Not Found')
+
