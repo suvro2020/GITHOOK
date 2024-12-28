d8ba3dd2ece3e836efb899baf7047dc73df2b187
Imranul Hasan <imranul.suvro@gmail.com>
2024-12-28 13:35:29 +0600

    New File Added.

---
+++ data_type.py
@@ -0,0 +1,4 @@
+print(type ("Hello Imranul, What is your Weight"))
+print (type (1550))
+print (type (15.67))
+print (type (False))
--- logical_operator.py
+++ logical_operator.py
@@ -1,3 +1,5 @@
 x = 5
 
 print(not(x > 3 and x < 10))
+
+print(not(x > 3 and x > 10))
